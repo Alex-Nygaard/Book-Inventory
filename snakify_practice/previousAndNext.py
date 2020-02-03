18ca1022d5e70ae047e93e89384e385c70f48b85
Alexander Nygaard <54611459+Alex-Nygaard@users.noreply.github.com>
2020-02-03 22:07:50 +0900

    Create previousAndNext.py

---
+++ snakify_practice/previousAndNext.py
@@ -0,0 +1,6 @@
+# Read an integer:
+a = int(input())
+
+# Print a+1 and a-1
+print("The next number for the number {} is {}.".format(a, a+1))
+print("The previous number for the number {} is {}.".format(a, a-1))
