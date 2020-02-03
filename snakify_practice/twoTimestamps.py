451c193e7d6bf2bdc89fa60f5dbd6b9a7702c06f
Alexander Nygaard <54611459+Alex-Nygaard@users.noreply.github.com>
2020-02-03 22:14:33 +0900

    Create twoTimestamps.py

---
+++ snakify_practice/twoTimestamps.py
@@ -0,0 +1,12 @@
+# Read integers:
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+
+
+time_between = f-c + (e-b) * 60 + (d-a) * 3600
+
+print(time_between)
